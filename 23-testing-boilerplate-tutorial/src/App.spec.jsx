d425e1a2dcd2b2e2ac262f72b3d03d93699046a8
Caroline de Marchi Petherson <104998076+Cahmarchi95@users.noreply.github.com>
2023-01-24 20:21:15 -0300

    chore: install jest and testing library

---
+++ 23-testing-boilerplate-tutorial/src/App.spec.jsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+describe("Jest", () => {
+  it("shoukd work", () => {
+    expect(1).toBe(1);
+  });
+
+  it("should display elements", () => {
+    render(<App />);
+
+    expect(
+      screen.getByRole("heading", { name: /vite \+ react/i })
+    ).toBeInTheDocument();
+  });
+});
